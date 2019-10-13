870ef097ea6949517912a2b2526721f3ad665666
Takken <newtakken@gmail.com>
2019-10-13 13:17:00 +0100

    commit

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 from app import app
 import os, bottle
+from flask import Flask
+from flask_ngrok import run_with_ngrok
 
+#app = Flask(__name__)
+#run_with_ngrok(app)
 if __name__ == '__main__':
-    bottle.TEMPLATE_PATH.insert(0, 'app/views/')  
-    app.run(host='localhost', port=8080, debug=True, reloader=True)
-        
+    bottle.TEMPLATE_PATH.insert(0, 'app/views/')
+    if os.environ.get('APP_LOCATION') == 'heroku':
+        app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    else:
+        app.run(host="localhost", port="8080", debug="True", reloader="True")
